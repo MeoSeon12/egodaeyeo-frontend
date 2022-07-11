637e6cbf3c6f0f495ba7cf4b9d952de1257029da
Roshu2 <kyumin1020@gmail.com>
2022-07-12 02:39:18 +0900

    ADD - modal animation

--- item/js/item.js
+++ item/js/item.js
@@ -1,14 +1,19 @@
 function openModal(){
     const body = document.getElementsByTagName('body')[0]
-    const modal = document.getElementsByClassName('modal-wrap')[0]
+    const modalWrap = document.getElementsByClassName('modal-wrap')[0]
+    const modalContainer = document.getElementsByClassName('modal-container')[0]
+    modalContainer.style.animation = 'roadRunnerIn 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
+    modalWrap.style.animation = 'fadeIn 2s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
+    modalWrap.style.display = 'flex'
     body.style.overflow = 'hidden'
-    modal.style.display = 'flex'
 }
 
 function closeModal(){
     const body = document.getElementsByTagName('body')[0]
-    const modal = document.getElementsByClassName('modal-wrap')[0]
-    modal.style.display = 'none'
+    const modalWrap = document.getElementsByClassName('modal-wrap')[0]
+    const modalContainer = document.getElementsByClassName('modal-container')[0]
+    modalContainer.style.animation = 'roadRunnerOut 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
+    modalWrap.style.animation = 'wrapRunnerOut 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
     body.style.overflow = 'auto'
 }
 
