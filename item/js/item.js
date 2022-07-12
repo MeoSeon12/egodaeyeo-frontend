b52d00e2f37bc5b1f1df7d5b65e2af1e679267e1
Roshu2 <kyumin1020@gmail.com>
2022-07-12 12:25:34 +0900

    DESIGN - item page

--- item/js/item.js
+++ item/js/item.js
@@ -1,23 +1,23 @@
 function openModal(){
     const body = document.getElementsByTagName('body')[0]
-    const modalWrap = document.getElementsByClassName('modal-wrap')[0]
-    const modalContainer = document.getElementsByClassName('modal-container')[0]
+    const modalWrap = document.getElementsByClassName('category-modal-wrap')[0]
+    const modalContainer = document.getElementsByClassName('category-modal-container')[0]
     modalContainer.style.animation = 'roadRunnerIn 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
     modalWrap.style.animation = 'fadeIn 2s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
     modalWrap.style.display = 'flex'
-    body.style.overflow = 'hidden'
+    // body.style.overflow = 'hidden'
 }
 
 function closeModal(){
     const body = document.getElementsByTagName('body')[0]
-    const modalWrap = document.getElementsByClassName('modal-wrap')[0]
-    const modalContainer = document.getElementsByClassName('modal-container')[0]
-    modalContainer.style.animation = 'roadRunnerOut 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
+    const modalWrap = document.getElementsByClassName('category-modal-wrap')[0]
+    const modalContainer = document.getElementsByClassName('category-modal-container')[0]
+    modalContainer.style.animation = 'scaleLeft 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
     modalWrap.style.animation = 'wrapRunnerOut 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards'
-    body.style.overflow = 'auto'
+    // body.style.overflow = 'auto'
 }
 
-const modalBody = document.querySelector('.modal-wrap')
+const modalBody = document.querySelector('.category-modal-wrap')
 modalBody.addEventListener('click', (e) => {
     if (e.target == modalBody) {
         closeModal()
