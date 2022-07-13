f5e8b23ba9e05fe8c8bcd170627905a26597674f
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-13 18:55:42 +0900

    FIX - Error Handling

--- item/js/detail_api.js
+++ item/js/detail_api.js
@@ -2,7 +2,7 @@ const frontendBaseUrl = "http://127.0.0.1:5500"
 const backendBaseUrl = "http://127.0.0.1:8000"
 
 async function DetailViewApi(itemId) {
-    
+
     const response = await fetch(`${backendBaseUrl}/items/details/${itemId}`, {
         method: 'GET',
         mode: 'cors',
@@ -11,15 +11,14 @@ async function DetailViewApi(itemId) {
         }
     })
 
-    //  요청 성공
+    //  요청 성공 (아이템 DB 존재함)
     if (response.status == 200) {
         data = await response.json()
         return data
     }
     
-    // 요청 실패
+    // 요청 실패 (아이템 DB 없음)
     else if (response.status == 404) {
-        data = await response.json()
-        return alert(data.msg)
+        return alert('아이템 정보가 존재하지 않습니다')
     }
 }
