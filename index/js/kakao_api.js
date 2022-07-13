05cbe96ccb543b2d51934091b7b4bc2b1e898c3f
KimmyJay <fkim0801@gmail.com>
2022-07-13 20:43:49 +0900

    FIX - Kakao Login JS

--- index/js/kakao_api.js
+++ index/js/kakao_api.js
@@ -17,7 +17,8 @@
 //     });
 // }
 
-
-// function kakaoLogin() {
-   
-// }
+const kakaoBtn = document.getElementsByClassName('kakao-btn')[0]
+kakaoBtn.addEventListener("click", (e) => {
+    window.location.href = `${backEndBaseUrl}/users/kakao/login`
+}
+)
