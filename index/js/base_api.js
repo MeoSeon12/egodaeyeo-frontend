63d4f02918114ed36ad0701e4d74cb1f24bfde3f
JaeWan CHOI <wkdudhksl@gamil.com>
2022-08-02 15:57:59 +0900

    REFACTOR - modified frontEndbaseUrl

--- index/js/base_api.js
+++ index/js/base_api.js
@@ -1,4 +1,4 @@
-const frontEndBaseUrl = "http://127.0.0.1:5500"
+const frontEndBaseUrl = "https://egodaeyeo.netlify.app"
 const backEndBaseUrl = "http://127.0.0.1:8000"
 
 function getCookie(name) {
